e4ff3f8329fd245dd0093625b313ba309ab55336
pratiwi9966 <wspratiwi2009@gmail.com>
2025-07-08 14:03:56 -0600

    added comments

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,23 +2,23 @@ using System;
 
 class Program
 {
+    // Main Function
     static void Main(string[] args)
     {
         DisplayWelcomeMessage();
-
         string userName = PromptUserName();
         int userNumber = PromptUserNumber();
-
         int squaredNumber = SquareNumber(userNumber);
-
         DisplayResult(userName, squaredNumber);
     }
 
+    // Function 1: Displays the message
     static void DisplayWelcomeMessage()
     {
         Console.WriteLine("\nWelcome to the program! ");
     }
 
+    // Function 2: Asks for and returns the user's name (as a string)
     static string PromptUserName()
     {
         Console.Write("Please enter your name: ");
@@ -27,6 +27,7 @@ class Program
         return name;
     }
 
+    // Function 3: Asks for and returns the user's favorite number (as an integer)
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
@@ -35,6 +36,7 @@ class Program
         return number;
     }
 
+    // Fuction 4: Accepts an integer as a parameter and returns that number squared (as an integer)
     static int SquareNumber(int number)
     {
         int square = number * number;
@@ -42,6 +44,7 @@ class Program
         return square;
     }
 
+    //Function 5: Accepts the user's name and the squared number and displays them.
     static void DisplayResult(string name, int square)
     {
         Console.WriteLine($"{name}, the aquare of your number is {square}");
