c6acc8ddb91b3f0447013ba50f348daa808f273c
pratiwi9966 <wspratiwi2009@gmail.com>
2025-07-10 15:30:27 -0600

    added common on Job.cs

--- week02/Resumes/Job.cs
+++ week02/Resumes/Job.cs
@@ -2,13 +2,15 @@ using System;
 
 public class Job
 {
+    //Member variables 
     public string _jobTitle;
     public string _company;
     public int _startYear;
     public int _endYear;
 
+    // Methods
     public void Display()
     {
-        Console.WriteLine($"{_jobTitle} ({ _company}) { _startYear}-{ _endYear}");
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
     }
 }
