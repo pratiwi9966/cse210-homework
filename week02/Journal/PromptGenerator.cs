bfde216113931ab4c97d6ea36574be8e88a7d1b0
pratiwi9966 <wspratiwi2009@gmail.com>
2025-07-09 20:24:55 -0600

    the structure

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,11 @@
+
+public class PrompGenerator
+{
+    public List<string> _prompts;
+
+
+    public string GetRandomPrompt()
+    {
+        return "";//return a enpty string, make this no error
+    }
+}
