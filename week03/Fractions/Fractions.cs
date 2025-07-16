fccebfce2b3ccf53ce82de83400f50b796186204
pratiwi9966 <wspratiwi2009@gmail.com>
2025-07-16 14:20:00 -0600

    added comment

--- week03/Fractions/Fractions.cs
+++ week03/Fractions/Fractions.cs
@@ -1,24 +1,25 @@
 using System;
 
 public class Fraction
-{
+{   // member variables
     private int _top;
     private int _bottom;
 
-    // Create Constructors
+    // Create Constructors  This is a default constructor.
+    // If no values are provided, the fraction defaults to 1/1.
     public Fraction()
     {
         // initializes the number to 1/1 or Default to 1/1
         _top = 1;
         _bottom = 1;
     }
-
+    // This allows creating a whole number as a fraction, like 5 → 5/1.
     public Fraction(int wholeNumber)
     {
         _top = wholeNumber;
         _bottom = 1; // initializes the denominator to 1
     }
-
+    // This allows full control: you pass both the numerator and denominator, e.g., 3/4.
     public Fraction(int top, int bottom)
     {
         _top = top;
@@ -31,7 +32,7 @@ public class Fraction
         string text = $"{_top} / {_bottom}";
         return text;
     }
-
+    // Calculates the decimal value of the fraction (e.g., 3/4 → 0.75).
     public double GetDecimalValue()
     {
         return (double)_top / (double)_bottom;
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,23 +1,35 @@
 using System;
 
 class Program
-{
+{   // The entry point of the program. This is where everything starts.
     static void Main(string[] args)
     {
+        // Creates a fraction f1 using the default constructor → 1/1.
         Fraction f1 = new Fraction();
+        // Prints "1 / 1".
         Console.WriteLine(f1.GetFractionString());
+        //Prints 1.0.
         Console.WriteLine(f1.GetDecimalValue());
 
+        // Creates a fraction f2 using the whole number constructor → 5/1.
         Fraction f2 = new Fraction(5);
+        // Prints "5 / 1".
         Console.WriteLine(f2.GetFractionString());
+        // Prints 5.0.
         Console.WriteLine(f2.GetDecimalValue());
 
+        // Creates a fraction f3 with top = 3 and bottom = 4 → 3/4.
         Fraction f3 = new Fraction(3,4);
+        // Prints "3 / 4".
         Console.WriteLine(f3.GetFractionString());
+        // Prints 0.75.
         Console.WriteLine(f3.GetDecimalValue());
 
+        //Creates a fraction f4 as 1/3.
         Fraction f4 = new Fraction(1,3);
+        // Prints "1 / 3".
         Console.WriteLine(f4.GetFractionString());
+        // Prints something like 0.3333333333333333.
         Console.WriteLine(f4.GetDecimalValue());
 
     }
