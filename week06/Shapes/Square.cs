6cc1f6d36d63413215d87c50626b3bb52155e813
pratiwi9966 <wspratiwi2009@gmail.com>
2025-08-06 15:24:29 -0600

    added comment

--- week06/Shapes/Square.cs
+++ week06/Shapes/Square.cs
@@ -2,7 +2,7 @@
 public class Square : Shape //nherit from Shape
 {
     // Member variable
-    private double _side;
+    private double _side; //stores the length of one side of the square
 
     //Constructor
     public Square(string color, double side) : base(color)
@@ -11,6 +11,9 @@ public class Square : Shape //nherit from Shape
     }
 
     // Method 
+    // The Shape class made this method abstract, which means every shape must write its own version.
+    // override means giving Square's version of GetArea() here.
+    // A square’s area is side × side, so we just return _side * _side.
     public override double GetArea()
     {
         return _side * _side;
